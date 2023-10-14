ede77a74436e5d7ab48dcfcaee1cb916510a23f3
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2023-10-14 17:54:56 +0200

    fix store import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import { AppProvider } from '../components/Context/Store'
+import { AppProvider } from '../components/Context/store'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
